32cf5416db550e3ff4d3c347d49e2fd3c8f39dd7
Jair Mendez <carlos@hadaelectronics.com>
2024-01-29 17:09:16 -0600

    Remove unused feature

--- kickstart/2023-personal-init.lua
+++ kickstart/2023-personal-init.lua
@@ -223,15 +223,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
