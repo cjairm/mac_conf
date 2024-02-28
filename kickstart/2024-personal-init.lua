8e535259f5d719b63474f7d2bcd35b8216a26f2f
Jair Mendez <carlos@hadaelectronics.com>
2024-02-27 19:03:03 -0600

    Revert

--- kickstart/2024-personal-init.lua
+++ kickstart/2024-personal-init.lua
@@ -291,7 +291,7 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VeryLazy', -- Sets the loading event to 'VeryLazy'
     config = function() -- This is the function that runs, AFTER loading
